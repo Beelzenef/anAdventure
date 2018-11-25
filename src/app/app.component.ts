365645ca582875a6d99b1fd05a40833df6874a25
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2018-11-25 15:22:28 +0100

    learn databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'anAdventure';
+
+  names : string[] = ["Name1", "Name2", "Name3"]
+
+  clickando(sentence : string) {
+    console.log("clicking: " + sentence)
+  }
 }
