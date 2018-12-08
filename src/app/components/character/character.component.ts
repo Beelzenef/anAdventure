28cc78b0a455c26f3e12b2d4d95f32d4073bea19
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2018-12-08 13:15:31 +0100

    complete start cmp (styles, redirect...)

--- src/app/components/character/character.component.ts
+++ src/app/components/character/character.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-
+    selector: "character-component",
+    templateUrl: "./character.component.html",
+    styleUrls: ["./character.component.css"]
 })
 export class CharacterComponent {
-    
 }
--- src/app/components/inventory/inventory.component.ts
+++ src/app/components/inventory/inventory.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-
+    selector: "inventory-component",
+    templateUrl: "./inventory.component.html",
+    styleUrls: ["./inventory.component.css"]
 })
 export class InventoryComponent {
     
